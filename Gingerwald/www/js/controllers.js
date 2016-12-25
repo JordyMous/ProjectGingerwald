621df99eecb397ec625d49790d19f43b63dd1271
JordyMous <jordymous@outlook.com>
2016-12-25 02:24:44 +0100

    Full Project (not finished)
    Fixed some typo's

---
+++ Gingerwald/www/js/controllers.js
@@ -0,0 +1,101 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  	// With the new view caching in Ionic, Controllers are only called
+  	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
+
+	// Form data for the login modal
+	$scope.loginData = {};
+
+	// Create the login modal that we will use later
+	$ionicModal.fromTemplateUrl('templates/login.html', {
+		scope: $scope
+	  	}).then(function(modal) {
+	    	$scope.modal = modal;
+	  	});
+
+	// Triggered in the login modal to close it
+	$scope.closeLogin = function() {
+		$scope.modal.hide();
+	};
+
+	// Open the login modal
+	$scope.login = function() {
+		$scope.modal.show();
+	};
+
+	// Perform the login action when the user submits the login form
+	$scope.doLogin = function() {
+		console.log('Doing login', $scope.loginData);
+
+	    // Simulate a login delay. Remove this and replace with your login
+	    // code if using a login system
+	    $timeout(function() {
+	    	$scope.closeLogin();
+	    }, 1000);
+	};
+})
+
+.controller ("DashboardCtrl", ['$scope', 'DashboardService', 'DateService', function ($scope, DashboardService, DateService) {
+	
+	//DEZE TOKEN KRIJG JE PAS TIJENS HET INLOGGEN, AANGEZIEN HET INLOGGEN NIET WERKT NAAR BEHOREN STAAT DEZE TIJDELIJK HIER OPGESLAGEN
+	$scope.token = "VRYsLhjqom93MPPPcfeaWwmb8S3hwS7rImoqS3OOVthP4BFApUPT1wIsW2UmSiFO";
+	$scope.data = "";
+	/**/$scope.response = "";
+
+	$scope.DOM = {
+		ingredients: document.getElementById ("ingredients"),
+		nutrients: document.getElementById ("nutrients")
+	};
+
+	$scope.today = new Date ();
+	$scope.firstDayWeek = DateService.getWeekDates ($scope.today)[0];
+	$scope.lastDayWeek = DateService.getWeekDates ($scope.today)[1];
+	$scope.firstDayMonth = DateService.getMonthDates ($scope.today)[0];
+	$scope.lastDayMonth = DateService.getMonthDates ($scope.today)[1];
+
+	$scope.dateFrom = $scope.firstDayMonth;
+	$scope.dateTo = $scope.lastDayMonth;
+
+
+	$scope.bottleToken = "f0uNmGdduGPsqo";
+
+	$scope.addShot = function () {
+		$scope.test = DashboardService.addShot ($scope.token, $scope.bottleToken);
+
+		$scope.test.then (function (data) {
+			$scope.response = data;
+			console.log ($scope.response);
+		});
+	};
+
+	$scope.getuserDashboard = function () {
+		$scope.dashboard = DashboardService.getDashboard ($scope.token, $scope.dateFrom, $scope.dateTo);
+
+		$scope.dashboard.then (function (data) {
+			$scope.data = data;
+			console.log ($scope.data.Shots);
+		});
+	};
+
+	$scope.showIngredients = function () {
+		//TIJDELIJK ZO, LATER VIA STYLE LATEN AANPASSEN EN EEN CSS DING MAKEN EN VISIBLE OP NONE ZET
+		//EVENTUEEL OOK MET VIEWS WERKEN
+		$scope.DOM.ingredients.style.display = "block";
+		$scope.DOM.nutrients.style.display = "none";
+	};
+
+	$scope.showNutrients = function () {
+		//TIJDELIJK ZO, LATER VIA STYLE LATEN AANPASSEN EN EEN CSS DING MAKEN EN VISIBLE OP NONE ZET
+		$scope.DOM.nutrients.style.display = "block";
+		$scope.DOM.ingredients.style.display = "none";
+	};
+
+	$scope.getuserDashboard ();
+	//$scope.addShot ();
+}])
---
+++ Gingerwald/www/js/services.js
@@ -0,0 +1,89 @@
+angular.module('starter.services', [])
+
+.service ('DashboardService', function($http, $q) {
+
+	return {
+		getDashboard : function (token, dateFrom, dateTo) {
+			var deferred = $q.defer ();
+			var url = 'https://www.gingerwald.com/community/v2.1/api/getUserDashboard.php?token=' + token + '&report_from=' + dateFrom + '&report_to=' + dateTo;
+      		
+			/**/console.log (url);
+
+      		$http({
+      			method: 'GET',
+        		url: url
+        	})
+      		.success (function (data, status, headers, config) {
+        		deferred.resolve (data);
+		    })
+		    .error (function (data, status, headers, config) {
+		    	deferred.reject (status);
+		    });
+
+			return deferred.promise;
+		},
+
+		addShot : function (token, bottleToken) {
+			var deferred = $q.defer ();
+			var url = 'https://www.gingerwald.com/community/v2.1/api/addBottleToDashboard.php?token=' + token + '&bottle_token=' + bottleToken;
+      		
+			/**/console.log (url);
+
+      		$http({
+      			method: 'POST',
+        		url: url
+        	})
+      		.success (function (data, status, headers, config) {
+        		deferred.resolve (data);
+		    })
+		    .error (function (data, status, headers, config) {
+		    	deferred.reject (status);
+		    });
+
+			return deferred.promise;
+		}
+	}
+})
+
+.service ('DateService', function () {
+
+	return {
+		formatDate : function (date) {
+			var year = date.getFullYear ();
+			var month = date.getMonth () + 1;
+			var day = date.getDate ();
+
+			return [year, month, day].join ('-');
+		},
+
+		getWeekDates : function (today) {
+			var firstDay = new Date ();
+			var lastDay = new Date ();
+
+			firstDay.setFullYear (today.getFullYear ());
+			firstDay.setMonth (today.getMonth ());
+			firstDay.setDate (today.getDate () - today.getDay () + 1);
+
+			lastDay.setFullYear (today.getFullYear ());
+			lastDay.setMonth (today.getMonth ());
+			lastDay.setDate (today.getDate () + 7 - today.getDay ());
+
+			return [this.formatDate (firstDay), this.formatDate (lastDay)];
+		},
+
+		getMonthDates : function (today) {
+			var firstDay = new Date ();
+			var lastDay = new Date ();
+
+			firstDay.setFullYear (today.getFullYear ());
+			firstDay.setMonth (today.getMonth ());
+			firstDay.setDate (1);
+
+			lastDay.setFullYear (today.getFullYear ());
+			lastDay.setMonth (today.getMonth () + 1); //+1 because day 0 returns the last day of the previous month
+			lastDay.setDate (0);
+
+			return [this.formatDate (firstDay), this.formatDate (lastDay)];
+		}
+	}
+});
